db9448f8a4fda4471d849f6856e4afa952f9772e
weihan1394 <weihan1394@gmail.com>
2020-05-25 15:48:15 +0800

    - update db module

--- src/ShippingService.Db/Scripts/Sql/S001_AddCarTypesTable.sql
+++ src/ShippingService.Db/Scripts/Sql/S001_AddCarTypesTable.sql
@@ -1,20 +1,11 @@
-CREATE TABLE [dbo].[CarTypes](
-	[Id] [int] IDENTITY(1,1) NOT NULL,
-	[Type] [varchar](20) NOT NULL,
- CONSTRAINT [PK_CarTypes] PRIMARY KEY CLUSTERED
-(
-	[Id] ASC
-)) ON [PRIMARY]
-GO
+CREATE TABLE IF NOT EXISTS CarTypes (
+    Id      SERIAL PRIMARY KEY,
+    Type    VARCHAR(50) NOT NULL
+);
 
-INSERT INTO [CarTypes] ([Type])
-VALUES ('Hatchback'), ('Kombi'), ('Sedan')
-GO
+INSERT INTO CarTypes (Type)
+VALUES
+       ('Hatchback'),
+       ('Kombi'),
+       ('Sedan');
 
-GRANT ALL ON CarTypes TO [user]
-GO
-
-ALTER TABLE [dbo].[Cars]
-    ADD [CarTypeId] INT NULL CONSTRAINT [FK_Cars_CarTypes] FOREIGN KEY([CarTypeId]) REFERENCES [dbo].[CarTypes] ([Id])
-    ON UPDATE CASCADE
-GO
